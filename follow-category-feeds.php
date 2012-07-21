d92e2f928c430525dc061c351b661ca1e14fae98
prasannasp <prasannasp@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-07-21 04:09:01 +0000

    Adding version 1.1 of Follow Category Feeds plugin. Fixed a deprecated function
    git-svn-id: https://plugins.svn.wordpress.org/follow-category-feeds/trunk@575378 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- follow-category-feeds.php
+++ follow-category-feeds.php
@@ -3,9 +3,26 @@
 Plugin Name: Follow Category Feeds
 Plugin URI: http://www.prasannasp.net/follow-wordpress-category-feeds-plugin/
 Description: This plugin adds link to RSS feed for the current post categories after post content. RSS feed link for categories are usually /category/categoryname/feed. Make sure you are not redirecting your category feeds to feedburner before activating this plugin.
-Version: 1.0
+Version: 1.1
 Author: Prasanna SP
 Author URI: http://www.prasannasp.net/
+*/
+
+/*  This file is part of Follow Category Feeds plugin, a Wordpress plugin written in PHP. 
+    Copyright 2012 Prasanna SP (email: prasanna@prasannasp.net)
+
+    Follow Category Feeds is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Follow Category Feeds is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Follow Category Feeds.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 function fwcf_follow_title() {
@@ -15,7 +32,7 @@ return 'Follow these topics:';
 function fwcf_add_to_post_footer($content) {
 	 if (is_single() )
  {
-		$content .= '<p>'.fwcf_follow_title().' '.fwcf_get_cat_feed_links().'</p>'; 
+		$content .= '<p class="fcfeeds">'.fwcf_follow_title().' '.fwcf_get_cat_feed_links().'</p>'; 
 			}
 	return $content;
 }
@@ -24,7 +41,7 @@ function fwcf_get_cat_feed_links() {
 $cats = get_the_category();
 foreach ($cats as $cat) {
 $catlinks[] = '<a href="'
-.get_category_rss_link(false,$cat->cat_ID,$cat->category_nicename)
+.get_category_feed_link(false,$cat->cat_ID,$cat->category_nicename)
 .'">'.$cat->cat_name.'</a>';
 }
 return implode(', ', $catlinks);
