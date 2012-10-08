4152ac30a859cda37c4f0b49060fbcb45e46aaf6
prasannasp <prasannasp@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-10-08 11:55:31 +0000

    Adding version 2.1.1 of follow category feeds plugin. Fixed a minor issue
    git-svn-id: https://plugins.svn.wordpress.org/follow-category-feeds/tags/2.1.1@609551 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- follow-category-feeds.php
+++ follow-category-feeds.php
@@ -3,7 +3,7 @@
 Plugin Name: Follow Category Feeds
 Plugin URI: http://www.prasannasp.net/follow-wordpress-category-feeds-plugin/
 Description: This plugin adds link to RSS feed for the current post categories after post content. RSS feed link for categories are usually /category/categoryname/feed. Make sure you are not redirecting your category feeds to feedburner before activating this plugin.
-Version: 1.1
+Version: 2.1.1
 Author: Prasanna SP
 Author URI: http://www.prasannasp.net/
 */
@@ -25,27 +25,106 @@ Author URI: http://www.prasannasp.net/
     along with Follow Category Feeds.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-function fwcf_follow_title() {
-return 'Follow these topics:';
-}
-
-function fwcf_add_to_post_footer($content) {
-	 if (is_single() )
- {
-		$content .= '<p class="fcfeeds">'.fwcf_follow_title().' '.fwcf_get_cat_feed_links().'</p>'; 
-			}
-	return $content;
-}
-
 function fwcf_get_cat_feed_links() {
 $cats = get_the_category();
 foreach ($cats as $cat) {
-$catlinks[] = '<a href="'
-.get_category_feed_link(false,$cat->cat_ID,$cat->category_nicename)
+$catfeedlinks[] = '<a href="'
+.get_category_feed_link($cat->cat_ID,'')
 .'">'.$cat->cat_name.'</a>';
 }
-return implode(', ', $catlinks);
+return implode(', ', $catfeedlinks);
+}
+
+function fwcf_add_to_post_footer($content) {
+	$options = get_option('fwcf_options');
+	$followcatstext = $options['follow_cats_txt'];
+
+ if (isset($options['on_other_pages']) && !(is_page() || is_attachment()) || is_single() )
+ {
+		$content .= '<p class="follow-cat-feed"> '.$followcatstext.' '.fwcf_get_cat_feed_links().'</p>'; 
+			}
+	return $content;
+
 }
 
 add_filter('the_content', 'fwcf_add_to_post_footer');
+
+// Set-up Action and Filter Hooks
+register_activation_hook(__FILE__, 'fwcf_add_defaults');
+add_action('admin_init', 'fwcf_init' );
+add_action('admin_menu', 'fwcf_add_options_page');
+add_filter('plugin_action_links', 'fwcf_plugin_action_links', 10, 2 );
+
+function fwcf_add_defaults() {
+	$tmp = get_option('fwcf_options');
+		$arr = array(	"follow_cats_txt" => "Follow these topics:"
+
+		);
+		update_option('fwcf_options', $arr);
+	}
+
+function fwcf_init(){
+	register_setting( 'fwcf_plugin_options', 'fwcf_options', 'fwcf_validate_options' );
+}
+
+function fwcf_add_options_page() {
+	add_options_page('Follow Category Feeds Options Page', 'Follow Category Feeds', 'manage_options', __FILE__, 'fwcf_options_page_form');
+}
+
+function fwcf_options_page_form() {
+	?>
+	<div class="wrap">
+
+		<div class="icon32" id="icon-options-general"><br></div>
+		<h2>Follow Category Feeds Options</h2>
+		<p>Set your options for Follow Category Feeds plugin.</p>
+
+		<form method="post" action="options.php">
+			<?php settings_fields('fwcf_plugin_options'); ?>
+			<?php $options = get_option('fwcf_options'); ?>
+
+			<table class="form-table">
+
+				<tr>
+					<th scope="row">Title for Follow Category Feeds:</th>
+					<td>
+						<input type="text" size="50" name="fwcf_options[follow_cats_txt]" value="<?php echo $options['follow_cats_txt']; ?>" />
+					</td>
+				</tr>
+				<tr valign="top">
+					<th scope="row">Show on other pages as well?</th>
+					<td>
+						<label><input name="fwcf_options[on_other_pages]" type="checkbox" value="1" <?php if (isset($options['on_other_pages'])) { checked('1', $options['on_other_pages']); } ?> /> <br />
+						<span class="description">By default the plugin shows category feed link on single post only. Selecting this will add category feed links to posts on other pages, such as blog page and archives where full content is shown.</span> </label><br />
+					</td>
+				</tr>
+			</table>
+			<p class="submit">
+			<input type="submit" class="button-primary" value="<?php _e('Save Changes') ?>" />
+			</p>
+		</form>
+<hr />
+<p style="margin-top:15px;font-size:12px;">If you have found this plugin is useful, please consider making a <a href="http://prasannasp.net/donate/" target="_blank">donation</a> to support the further development of this plugin. Thank you!</p>
+	</div>
+	<?php	
+}
+
+// Sanitize and validate input
+function fwcf_validate_options($input) {
+	 // strip html from textboxes
+	$input['follow_cats_txt'] =  wp_filter_nohtml_kses($input['follow_cats_txt']); // strip html tags, and escape characters
+	return $input;
+}
+
+// Display a Settings link on the main Plugins page
+function fwcf_plugin_action_links( $links, $file ) {
+
+	if ( $file == plugin_basename( __FILE__ ) ) {
+		$fwcf_links = '<a href="'.get_admin_url().'options-general.php?page=follow-category-feeds/follow-category-feeds.php">'.__('Settings').'</a>';
+		// make the 'Settings' link appear first
+		array_unshift( $links, $fwcf_links );
+	}
+
+	return $links;
+}
 ?>
